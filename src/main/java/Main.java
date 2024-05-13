7392852b8d89cae1db5efe1e384b29ada249ee1a
MonicaMoren <169068483+MonicaMoren@users.noreply.github.com>
2024-05-13 18:33:08 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,54 @@
+ public class Main {
+    public static void main(String[] args) {
+      System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+      System.out.println("**********************");
+      System.out.println("*Artista:Taylor Swift*");
+      System.out.println("**********************");
+      System.out.println(" ");
+      System.out.println("##############################");
+      System.out.println("# Álbum da Taylor Swift 1989 #");
+      System.out.println("##############################");
+      System.out.println(" ");
+      System.out.println("Lançamento do Álbum 1989");
+      System.out.println("27 de outubro de 2014");
+      System.out.println(" ");
+      System.out.println("==============================================================");
+
+      System.out.println("Lançado em 27 de outubro, ele já é a maior lançamento de álbum de 2023 no Spotify e a segunda maior estreia na plataforma. “1989 Taylor's Version” perde apenas para “Midnights”, da própria Taylor Swift, como maior estreia do Spotify.");
+      System.out.println("============================================================== ");
+      System.out.println(" ");
+      System.out.println("+++++++++++++++++++++++");
+      System.out.println("+ Minutagem e Músicas +");
+      System.out.println("+++++++++++++++++++++++");
+      System.out.println("  ");
+
+      System.out.println("1 - Welcome To New York Minutos 3:33");
+      System.out.println(" ");
+      System.out.println("2 - Style Minutos 3:51 ");
+      System.out.println(" ");
+      System.out.println("3 - Blank Space Minutos 3:52");    
+      System.out.println(" ");
+      System.out.println("4 - Out of the Woods Minutos 3:56");
+      System.out.println(" ");
+      System.out.println("5 - All you had to do was stay Minutos 3:13");
+      System.out.println(" ");
+      System.out.println("6 - Shake It Off Minutagem 3:39");
+      System.out.println(" ");
+      System.out.println("7 - I Wish You Would Minutagem 3:27");
+      System.out.println(" ");
+      System.out.println("8 - Bad Blood  Minutagem 3:32");
+      System.out.println(" ");
+      System.out.println("9 - Wildest Dreams Minutagem 3:40");
+      System.out.println(" ");
+      System.out.println("10 - How You Get yhe Girl Minutagem 4:08");
+      System.out.println(" ");
+      System.out.println("11 - This Love MInutos 4:10 ");
+      System.out.println(" ");
+      System.out.println("12 - I Know Plances Minutagem 3:16");
+      System.out.println(" ");
+      System.out.println("13 - Clean Minutagem 4:31");
+      System.out.println(" ");
+      System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+    }
+  }
